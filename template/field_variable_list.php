b0c4775b6690d481a5e456333e053c56c33c8dbd
tertek <ekin.tertemiz@swisstph.ch>
2021-07-20 08:41:19 +0200

    Sanitize array.

--- template/field_variable_list.php
+++ template/field_variable_list.php
@@ -4,10 +4,18 @@
  * 
  */
 
-$response = "";
-$fields = htmlentities()($_POST["fields"]);
+function sanitize($arg) {
+    if (is_array($arg)) {
+        return array_map('sanitize', $arg);
+    }
 
-foreach ($fields as $key => $field) {
+    return htmlentities($arg, ENT_QUOTES, 'UTF-8');
+}
+
+$response = "";
+$fields = sanitize($_POST["fields"]);
+
+foreach ($fields as $key => $field ) {
 
     if( empty($field["fieldValue"])) {
         $state = "is-empty";
@@ -23,10 +31,10 @@ foreach ($fields as $key => $field) {
     $response .= '<div class="col-auto">';
     $response .= '<label class="sr-only" for="inlineFormInputGroup"></label>';
     $response .= '<div class="input-group mb-2">';
-    $response .= '<div class="input-group-prepend"><div class="input-group-text">'.htmlspecialchars($field["fieldName"]).'</div></div>';
-    $response .= '<input type="text" class="form-control '.$state.' variable-input" name="fields['.htmlspecialchars($field["fieldName"]).']" id="fieldVariableMatch-'.htmlspecialchars($key).'" value="'.htmlspecialchars($field["fieldValue"]).'" placeholder="" onfocusout="STPH_pdfInjector.validateField('.htmlspecialchars($key).');">';
+    $response .= '<div class="input-group-prepend"><div class="input-group-text">'.$field["fieldName"].'</div></div>';
+    $response .= '<input type="text" class="form-control '.$state.' variable-input" name="fields['.$field["fieldName"].']" id="fieldVariableMatch-'.htmlspecialchars($key).'" value="'.htmlspecialchars($field["fieldValue"]).'" placeholder="" onfocusout="STPH_pdfInjector.validateField('.htmlspecialchars($key).');">';
     $response .= '</div></div><div class="col-auto"><div class="form-check mb-2"> <label class="form-check-label" for="autoSizingCheck">';
-    $response .= '<small id="variableHelpLine-'.htmlspecialchars($key).'" class="text-'.$class.'">'.$text.'</small>';
+    $response .= '<small id="variableHelpLine-'.$key.'" class="text-'.$class.'">'.$text.'</small>';
     $response .= '</label></div></div></div>';
 }
 
