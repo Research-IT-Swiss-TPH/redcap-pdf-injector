68f41d49ca4e316ab2f7e317baa10956daf251d3
tertek <ekin.tertemiz@swisstph.ch>
2022-02-22 12:32:55 +0100

    Improve project creation.

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -2,6 +2,7 @@
 
 use \ExternalModules\ExternalModules;
 use \Exception;
+use \Project;
 
 require_once __DIR__ . '/../../../redcap_connect.php';
 
@@ -62,20 +63,23 @@ abstract class BaseTest extends \ExternalModules\ModuleBaseTest {
 
         $sql = "insert into redcap_config values ('external_modules_test_pids', ?)";
         
-        $result = ExternalModules::query($sql, [$value]);
+        ExternalModules::query($sql, [$value]);
     }
 
     
     //  Create test project
     static function createTestProject($title){
 
-        $title = \Project::cleanTitle($title);
-        $new_app_name = \Project::getValidProjectName($title);
+        $title = Project::cleanTitle($title);
+        $new_app_name = Project::getValidProjectName($title);
 
         $purpose = 0;
         $project_note = 'Unit Testing';
 
-        $ui_id = NULL;
+        //$ui_id = NULL;
+        $ui_id = 1;
+        $ui_name = NULL;
+        $ui_name = 'site_admin';
         $auto_inc_set = 1;
         $GLOBALS['__SALT__'] = substr(sha1(rand()), 0, 10);
 
@@ -83,12 +87,20 @@ abstract class BaseTest extends \ExternalModules\ModuleBaseTest {
             [$new_app_name,$purpose,$title,NOW,$ui_id,$auto_inc_set,trim($project_note),'none',$GLOBALS['__SALT__']]);            
 
         // Get this new project's project_id
-        $pid = db_insert_id();
+        $new_project_id = db_insert_id();
 
         // Insert project defaults into redcap_projects
-        \Project::setDefaults($pid);
+        Project::setDefaults($new_project_id);
 
-        return $pid;
+		Project::insertDefaultArmAndEvent($new_project_id);
+
+        // Now add the new project's metadata
+		$form_names = createMetadata($new_project_id);
+
+        // Insert user rights for this new project for user with user id = 1
+        Project::insertUserRightsProjectCreator($new_project_id, $ui_name, 0, 0, $form_names);
+
+        return $new_project_id;
 
     }
         
