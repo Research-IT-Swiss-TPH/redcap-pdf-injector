9d37921faf57d7eb8de56a17ccd32020e14b6f90
tertek <ekin.tertemiz@swisstph.ch>
2022-03-04 14:10:41 +0100

    Revert "Add module CSRF check."
    This reverts commit 286d7831f80c126432ed34de4516ac1c0ef09510.

--- tests/RequestHandlerTest.php
+++ tests/RequestHandlerTest.php
@@ -7,23 +7,26 @@ use \HttpClient;
 use \Exception;
 use \GuzzleHttp\Psr7;
 
+
 final class RequestHandlerTest extends BaseTest {
 
+
     /**
+     * scanFile()
+     * 
      * @since 1.4.0
      */
+
     function testScanFile_throws_for_no_file_set() {
 
-
+        $client = new HttpClient;
         $requestURL = $this->getUrl("requestHandler.php") . "&action=fileScan";
 
         $this->expectExceptionMessage("Unknown Error");
-        HttpClient::request('post', $requestURL);        
+        $client::request('post', $requestURL);        
         
     }
-    /**
-     * @since 1.4.0
-     */
+
     function testScanFile_throws_for_invalid_file() {
 
         $client = new HttpClient;
@@ -41,9 +44,7 @@ final class RequestHandlerTest extends BaseTest {
             ]
         ]);
     }
-    /**
-     * @since 1.4.0
-     */
+
     function testScanFile_throws_for_invalid_pdf(){
         $client = new HttpClient;
         $requestURL = $this->getUrl("requestHandler.php") . "&action=fileScan";
@@ -59,9 +60,7 @@ final class RequestHandlerTest extends BaseTest {
             ]
         ]);
     }
-    /**
-     * @since 1.4.0
-     */
+
     function testScanFile_succeeds(){
         $client = new HttpClient;
         $requestURL = $this->getUrl("requestHandler.php") . "&action=fileScan";
@@ -83,110 +82,41 @@ final class RequestHandlerTest extends BaseTest {
     }
 
     /**
-     * @since 1.4.1
-     */
-    function testScanField_auth_fails_without_token() {
-        $client = new HttpClient;
-        $requestURL = $this->getUrl("requestHandler.php") . "&action=fieldScan";
-
-        $this->expectExceptionMessageMatches("/Token not set/");
-        $response = $client::request('post', $requestURL, []);
-
-    }
-
-    /**
-     * @since 1.4.1
-     */
-    function testScanField_auth_fails_with_wrong_token() {
-        $client = new HttpClient;
-        $requestURL = $this->getUrl("requestHandler.php") . "&action=fieldScan";
-
-        $this->expectExceptionMessageMatches("/Wrong number of segments/");
-        $response = $client::request('post', $requestURL, [
-            'form_params' => [
-                "pdfi_jwt" => "FooBar"
-            ]
-        ]);
-
-    }
-
-    /**
-     * @since 1.4.1
-     */
-    function testScanField_fails_with_expired_token() {
-        $client = new HttpClient;
-        $requestURL = $this->getUrl("requestHandler.php") . "&action=fieldScan";
-
-        $token = $this->getTestToken(false);
-
-        $this->expectExceptionMessageMatches("/Token has expired./");
-        $client::request('post', $requestURL, [
-            'form_params' =>
-            [
-                "pdfi_jwt" => $token
-            ]
-        ]);
-
-    }
-
-       /**
-     * @since 1.4.1
-     */
-    function testScanField_fails_with_valid_token() {
-        $client = new HttpClient;
-        $requestURL = $this->getUrl("requestHandler.php") . "&action=fieldScan";
-
-        $token = $this->getTestToken(false);
-
-        $this->expectExceptionMessage("Field is invalid");
-        $client::request('post', $requestURL, [
-            'form_params' =>
-            [
-                "pdfi_jwt" => $token
-            ]
-        ]);
-
-    }     
-
-    /**
+     * scanField()
+     * 
      * @since 1.4.0
      */
     function testScanField_fails_for_no_fieldName_set() {
         $client = new HttpClient;
         $requestURL = $this->getUrl("requestHandler.php") . "&action=fieldScan";
         
-        //$this->expectExceptionMessage("Field is invalid");
-        $response = $client::request('post', $requestURL, []);
-        $this->assertSame(401, $response->getStatusCode());
+        $this->expectExceptionMessage("Field is invalid");
+        $client::request('post', $requestURL, []);
+
     }
-    /**
-     * @since 1.4.0
-     */
+
     function testScanField_fails_for_invalid_fieldName_set() {
         $client = new HttpClient;
         $requestURL = $this->getUrl("requestHandler.php") . "&action=fieldScan";
         
-        //$this->expectExceptionMessage("Field is invalid");
-        $response = $client::request('post', $requestURL, [
+        $this->expectExceptionMessage("Field is invalid");
+        $client::request('post', $requestURL, [
             "fieldName" => "non-existing-field"
         ]);
-        $this->assertSame(401, $response->getStatusCode());
+
     }
-    /**
-     * @since 1.4.0
-     */
+
     function testScanField_succeeds() {
         $client = new HttpClient;
 
         //  We have to send pid
-        $requestURL = $this->getUrl("requestHandler.php") . "&action=fieldScan";
+        $requestURL = $this->getUrl("requestHandler.php") . "&action=fieldScan&pid=" . PROJECT_ID;
         
-        //$this->expectExceptionMessage("Field is invalid");
         $response = $client::request('post', $requestURL, [
             "fieldName" => "'record_id'"
         ]);
-        $this->assertSame(401, $response->getStatusCode());
-        //$this->assertSame(200, $response->getStatusCode());
+
+        $this->assertSame(200, $response->getStatusCode());
     }   
 
 }
